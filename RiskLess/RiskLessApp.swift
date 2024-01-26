0dc704ae514ee245908b5ac3322c55f7f776a0ce
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-01-26 15:42:48 +0500

    Initial Commit

---
+++ RiskLess/RiskLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiskLessApp.swift
+//  RiskLess
+//
+//  Created by Akbar Khusanbaev on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RiskLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
