52d3c1b02f8a39c432e8347fc17c34dd2b14707e
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-01-27 18:00:50 +0500

    fiexed a bug

--- RiskLess/Paywall/PaywallView.swift
+++ RiskLess/Paywall/PaywallView.swift
@@ -17,65 +17,76 @@ struct PaywallView: View {
     var body: some View {
         VStack {
             if let product = paywallVM.products.first {
-                ScrollView {
-                    HStack {
-                        Button("Cancel") {
-                            dismiss()
+                ZStack {
+                    VStack {
+                        ScrollView {
+                            HStack {
+                                Button("Cancel") {
+                                    dismiss()
+                                }
+                                Spacer()
+                            }
+                            .padding()
+                            HStack {
+                                Text("👑 " + product.displayName + " " + product.description)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.center)
+                            }
+                            .padding()
+                            HStack {
+                                Text("Get premium access to all features for just \(product.displayPrice)/month")
+                                    .font(.caption)
+                                    .multilineTextAlignment(.center)
+                            }
+                            .padding()
+                            if isEligibleForOffer {
+                                Text("You can start your 1 month free trial. And pay \(product.displayPrice) after subscription")
+                                    .font(.headline)
+                            }
+                            
                         }
-                        Spacer()
-                    }
-                    .padding()
-                    HStack {
-                        Text("👑 " + product.displayName + " " + product.description)
-                            .font(.headline)
-                            .multilineTextAlignment(.center)
-                    }
-                    .padding()
-                    HStack {
-                        Text("Get premium access to all features for just \(product.displayPrice)/month")
-                            .font(.caption)
-                            .multilineTextAlignment(.center)
-                    }
-                    .padding()
-                    if isEligibleForOffer {
-                        Text("You can start your 1 month free trial. And pay \(product.displayPrice) after subscription")
-                            .font(.headline)
+                        Button {
+                            Task {
+                                do {
+                                    try await paywallVM.purchase(product)
+                                    paid = try await paywallVM.isPurchased(product)
+                                } catch {
+                                    // Handle errors later
+                                }
+                            }
+                        } label: {
+                            if isEligibleForOffer {
+                                Text("Start your monthly Trial")
+                                    .font(.headline)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10.0)
+                                            .fill(Color.accentColor.gradient)
+                                    )
+                                
+                            } else {
+                                Text("Continue for \(product.displayPrice)")
+                                    .font(.headline)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10.0)
+                                            .fill(Color.accentColor.gradient)
+                                    )
+                            }
+                        }
+                        .foregroundStyle(.background)
+                        .padding()
                     }
                     
-                }
-                Button {
-                    Task {
-                        do {
-                            try await paywallVM.purchase(product)
-                            paid = try await paywallVM.isPurchased(product)
-                        } catch {
-                            // Handle errors later
+                    if paywallVM.purchasing {
+                        VStack {
+                            ProgressView()
                         }
-                    }
-                } label: {
-                    if isEligibleForOffer {
-                        Text("Start your monthly Trial")
-                            .font(.headline)
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10.0)
-                                    .fill(Color.accentColor.gradient)
-                            )
-
-                    } else {
-                        Text("Continue for \(product.displayPrice)")
-                            .font(.headline)
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10.0)
-                                    .fill(Color.accentColor.gradient)
-                            )
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
                 }
-                .foregroundStyle(.background)
-                .padding()
             }
         }
         .onChange(of: paid, perform: { newValue in
@@ -88,6 +99,7 @@ struct PaywallView: View {
             self.isEligibleForOffer = await paywallVM.isEligibleForFreeTrial()
         })
         .presentationDetents([.medium])
+        .interactiveDismissDisabled(paywallVM.purchasing)
     }
 }
 
